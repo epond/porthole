ed006cebf5cc160c78ddc7bba3c91b4ab4d6336d
epond <epond@users.noreply.github.com>
2016-07-26 08:29:37 +0100

    Add skeleton implementation

---
+++ web.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Print("Starting porthole...")
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/dashboard", DashboardHandler)
+
+	http.ListenAndServe("localhost:9000", nil)
+}
+
+func HomeHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "GET /dashboard (see project readme for more information)")
+}
+
+func DashboardHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "Dashboard will appear here")
+}

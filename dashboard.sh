d0b7ca88b2386d56d6a9ec581517e48eb294c37d
epond <epond@users.noreply.github.com>
2016-07-30 17:06:02 +0100

    Update dashboard.sh

--- dashboard.sh
+++ dashboard.sh
@@ -7,6 +7,7 @@ echo "Starting porthole in background..."
 /home/pi/go/bin/porthole &
 
 echo "Starting browser in kiosk mode..."
+# https://github.com/elalemanyo/raspberry-pi-kiosk-screen
 sudo -u pi /usr/bin/epiphany-browser -a -i --profile ~/.config http://localhost:9000 --display=:0 &
 sleep 10s;
 xte "key F11" -x:0

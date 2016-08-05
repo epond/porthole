8f4af2ce2007616f7980cad10fa755875fe67b5c
Edward Pond <edward.pond@springer.com>
2016-08-05 17:10:00 +0100

    TestGivenDepthOfOneThenReturnSubfolderInfo

--- folderScanner_test.go
+++ folderScanner_test.go
@@ -14,7 +14,22 @@ func TestGivenZeroDepthThenReturnEmptyArray(t *testing.T) {
 
 func TestGivenDepthOfOneThenReturnSubfolderInfo(t *testing.T) {
 	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
-	if len(FileInfoAtDepth(folderPath, 1)) != 3 {
+	fileInfos := FileInfoAtDepth(folderPath, 1)
+	if len(fileInfos) != 3 {
 		t.Error("File info array at depth 1 did not have length 3")
+	}
+	if fileInfos[0].Name() != "a2" || fileInfos[1].Name() != "b2" || fileInfos[2].Name() != "c2" {
+		t.Error("Folder name was not as expected")
+	}
+}
+
+func TestGivenDepthOfTwoThenReturnSubfolderInfo(t *testing.T) {
+	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
+	fileInfos := FileInfoAtDepth(folderPath, 2)
+	if len(fileInfos) != 4 {
+		t.Error("File info array at depth 2 did not have length 4")
+	}
+	if fileInfos[0].Name() != "a3b2" || fileInfos[1].Name() != "b3b2" || fileInfos[2].Name() != "a3c2" || fileInfos[3].Name() != "b3c2" {
+		t.Error("Folder name was not as expected")
 	}
 }

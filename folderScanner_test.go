c9631615beabcab0788b32f118b71143999a763a
Edward Pond <edward.pond@springer.com>
2016-08-05 16:57:42 +0100

    TestGivenDepthOfOneThenReturnSubfolderInfo

--- folderScanner_test.go
+++ folderScanner_test.go
@@ -1,9 +1,20 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"path"
+	"os"
+)
 
-func TestGivenZeroDepthThenFileInfoAtDepthReturnsEmptyArray(t *testing.T) {
+func TestGivenZeroDepthThenReturnEmptyArray(t *testing.T) {
 	if len(FileInfoAtDepth("anything", 0)) != 0 {
-		t.Error("not implemented")
+		t.Error("File info array at depth 0 was non-empty")
+	}
+}
+
+func TestGivenDepthOfOneThenReturnSubfolderInfo(t *testing.T) {
+	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
+	if len(FileInfoAtDepth(folderPath, 1)) != 3 {
+		t.Error("File info array at depth 1 did not have length 3")
 	}
 }

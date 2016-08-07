d16a273dda00b1ad699d117ac81ef08dade43a01
epond <epond@users.noreply.github.com>
2016-08-07 11:34:49 +0100

    sort in test to overcome mac/linux differences

--- folderScanner_test.go
+++ folderScanner_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"bytes"
+	"sort"
 )
 
 func TestGivenZeroDepthThenReturnEmptyArray(t *testing.T) {
@@ -14,16 +15,16 @@ func TestGivenZeroDepthThenReturnEmptyArray(t *testing.T) {
 
 func TestGivenDepthOfOneThenReturnSubfolderInfo(t *testing.T) {
 	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
-	fileInfos := FileInfoAtDepth(folderPath, 1)
+	fileInfos := sortByName(FileInfoAtDepth(folderPath, 1))
 	expect(t, "number of fileinfos", "3", strconv.Itoa(len(fileInfos)))
 	expect(t, "foldernames", "a2|b2|c2", fileInfoNames(fileInfos))
 }
 
 func TestGivenDepthOfTwoThenReturnSubfolderInfo(t *testing.T) {
 	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
-	fileInfos := FileInfoAtDepth(folderPath, 2)
+	fileInfos := sortByName(FileInfoAtDepth(folderPath, 2))
 	expect(t, "number of fileinfos", "4", strconv.Itoa(len(fileInfos)))
-	expect(t, "foldernames", "a3b2|b3b2|a3c2|b3c2", fileInfoNames(fileInfos))
+	expect(t, "foldernames", "a3b2|a3c2|b3b2|b3c2", fileInfoNames(fileInfos))
 }
 
 func expect(t *testing.T, valueName string, expected string, actual string) {
@@ -41,4 +42,23 @@ func fileInfoNames(fileInfos []os.FileInfo) string {
 		}
 	}
 	return allnames.String()
+}
+
+type FileInfos []os.FileInfo
+
+func (slice FileInfos) Len() int {
+	return len(slice)
+}
+
+func (slice FileInfos) Less(i, j int) bool {
+	return slice[i].Name() < slice[j].Name();
+}
+
+func (slice FileInfos) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
+
+func sortByName(fileInfos FileInfos) FileInfos {
+	sort.Sort(fileInfos)
+	return fileInfos
 }

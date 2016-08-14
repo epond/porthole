e43156db6b4c8ff703c8b81db1d5e54fe136e1fb
epond <epond@users.noreply.github.com>
2016-08-14 17:55:55 +0100

    ScanFolders used in tests

--- folderScanner_test.go
+++ folderScanner_test.go
@@ -16,14 +16,14 @@ func TestGivenZeroDepthThenReturnEmptyArray(t *testing.T) {
 // A folder such as a/b needs to have entry "a - b" where a is artist and b is name of release
 func TestScanListEntriesContainTwoFolderLevels(t *testing.T) {
 	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
-	folderInfos := sortFolderInfoByString(FolderInfoAtDepth(FolderToScan{folderPath, 1}))
+	folderInfos := sortFolderInfoByString(ScanFolders([]FolderToScan{{folderPath, 1}}))
 	expectInt(t, "number of folderInfos", 3, len(folderInfos))
 	expect(t, "folderInfo strings", "a1 - a2|a1 - b2|a1 - c2", pipeDelimitedString(folderInfos))
 }
 
 func TestScanListAtGreaterDepth(t *testing.T) {
 	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
-	folderInfos := sortFolderInfoByString(FolderInfoAtDepth(FolderToScan{folderPath, 2}))
+	folderInfos := sortFolderInfoByString(ScanFolders([]FolderToScan{{folderPath, 2}}))
 	expectInt(t, "number of folderInfos", 4, len(folderInfos))
 	expect(t, "folderInfo strings", "b2 - a3b2|b2 - b3b2|c2 - a3c2|c2 - b3c2", pipeDelimitedString(folderInfos))
 }

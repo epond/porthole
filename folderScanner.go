783445761fa661c59d6dcecee58c3ba990e5951c
epond <epond@users.noreply.github.com>
2016-08-14 11:28:13 +0100

    add FolderInfo which knows about parent

--- folderScanner.go
+++ folderScanner.go
@@ -48,6 +48,48 @@ func FileInfoAtDepth(rootFolderPath string, targetDepth int) []os.FileInfo {
 	return fileInfos
 }
 
+func FolderInfoAtDepthIter(rootFolderPath string, targetDepth int, parent os.FileInfo) []FolderInfo {
+	if targetDepth <= 0 {
+		return []FolderInfo{}
+	}
+
+	rootFile, err := os.Open(rootFolderPath)
+	if err != nil {
+		log.Fatalf("Could not open root folder. Cause: %v", err)
+	}
+	children, err := rootFile.Readdir(0)
+	if err != nil {
+		log.Fatalf("Could not read root folder info. Cause: %v", err)
+	}
+	if parent == nil {
+		rootFileInfo, err := rootFile.Stat()
+		if err != nil {
+			log.Fatalf("Could not read Stat on root folder. Cause: %v", err)
+		}
+		parent = rootFileInfo
+	}
+
+	folderInfos := make([]FolderInfo, 0)
+
+	for _, child := range children {
+		firstChar, _ := utf8.DecodeRuneInString(child.Name())
+		if child.IsDir() && child.Name() != "@eaDir" && firstChar != '.' {
+			if targetDepth == 1 {
+				folderInfos = append(folderInfos, FolderInfo{child, parent})
+			} else {
+				childFolderInfos := FolderInfoAtDepthIter(path.Join(rootFolderPath, child.Name()), targetDepth - 1, child)
+				folderInfos = append(folderInfos, childFolderInfos...)
+			}
+		}
+	}
+
+	return folderInfos
+}
+
+func FolderInfoAtDepth(rootFolderPath string, targetDepth int) []FolderInfo {
+	return FolderInfoAtDepthIter(rootFolderPath, targetDepth, nil)
+}
+
 func LatestFileInfos(fileInfos []os.FileInfo, limit int) []os.FileInfo {
 	sortedFileInfos := sortByModTime(fileInfos)
 	if len(sortedFileInfos) > limit {
@@ -73,4 +115,16 @@ func (slice FileInfosSortedByModifiedTime) Swap(i, j int) {
 func sortByModTime(fileInfos FileInfosSortedByModifiedTime) FileInfosSortedByModifiedTime {
 	sort.Sort(fileInfos)
 	return fileInfos
+}
+
+type FolderInfo struct {
+	fileInfo os.FileInfo
+	parent os.FileInfo
+}
+
+func (f *FolderInfo) String() string {
+	if f.parent == nil {
+		return f.fileInfo.Name()
+	}
+	return fmt.Sprintf("%v - %v", f.parent.Name(), f.fileInfo.Name())
 }
--- folderScanner_test.go
+++ folderScanner_test.go
@@ -51,6 +51,21 @@ func TestLatestFileInfosOrdersByModifiedTime(t *testing.T) {
 	expect(t, "folder names", "file1|file4|file3", fileInfoNames(LatestFileInfos(rawFileInfos, 3)))
 }
 
+// A folder such as a/b needs to have entry "a - b" where a is artist and b is name of release
+func TestScanListEntriesContainTwoFolderLevels(t *testing.T) {
+	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
+	folderInfos := sortFolderInfoByString(FolderInfoAtDepth(folderPath, 1))
+	expectInt(t, "number of folderInfos", 3, len(folderInfos))
+	expect(t, "folderInfo strings", "a1 - a2|a1 - b2|a1 - c2", pipeDelimitedString(folderInfos))
+}
+
+func TestScanListAtGreaterDepth(t *testing.T) {
+	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
+	folderInfos := sortFolderInfoByString(FolderInfoAtDepth(folderPath, 2))
+	expectInt(t, "number of folderInfos", 4, len(folderInfos))
+	expect(t, "folderInfo strings", "b2 - a3b2|b2 - b3b2|c2 - a3c2|c2 - b3c2", pipeDelimitedString(folderInfos))
+}
+
 func expect(t *testing.T, valueName string, expected string, actual string) {
 	if actual != expected {
 		t.Errorf("Expected %v to be %v but was %v", valueName, expected, actual)
@@ -70,6 +85,17 @@ func fileInfoNames(fileInfos []os.FileInfo) string {
 		}
 	}
 	return allnames.String()
+}
+
+func pipeDelimitedString(list []FolderInfo) string {
+	var all bytes.Buffer
+	for i, element := range list {
+		all.WriteString(element.String())
+		if i < len(list) - 1 {
+			all.WriteString("|")
+		}
+	}
+	return all.String()
 }
 
 type FileInfosSortedByName []os.FileInfo
@@ -121,4 +147,23 @@ func (d DummyFileInfo) IsDir() bool {
 
 func (d DummyFileInfo) Sys() interface{} {
 	return nil
+}
+
+type FolderInfosSortedByString []FolderInfo
+
+func (slice FolderInfosSortedByString) Len() int {
+	return len(slice)
+}
+
+func (slice FolderInfosSortedByString) Less(i, j int) bool {
+	return slice[i].String() < slice[j].String();
+}
+
+func (slice FolderInfosSortedByString) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
+
+func sortFolderInfoByString(folderInfos FolderInfosSortedByString) FolderInfosSortedByString {
+	sort.Sort(folderInfos)
+	return folderInfos
 }

2fb84882dc2dfd1b4458a31941b9266df81787a1
epond <epond@users.noreply.github.com>
2016-08-14 22:58:16 +0100

    add releasesRepo failing tests

--- releasesRepo_test.go
+++ releasesRepo_test.go
@@ -4,19 +4,56 @@ import (
 	"testing"
 	"os"
 	"path"
+	"bufio"
 )
 
 func TestWhenFileMissingItCreatesFile(t *testing.T) {
 	setUp()
 	defer tearDown()
-	UpdateKnownReleases([]FolderInfo{{DummyFileInfo{"Lieutenant Pigeon", false}, nil}}, knownReleasesFile(), 0)
+	UpdateKnownReleases([]FolderInfo{{DummyFileInfo{"Lieutenant Pigeon", true}, nil}}, knownReleasesFile(), 0)
 	_, err := os.Stat(knownReleasesFile())
 	if (err != nil) {
 		t.Error("Expected UpdateKnownReleases to create known releases file but it didn't")
 	}
 }
 
-//func TestItAddsReleasesToEndOfFile(t *testing.T) {}
+func TestItAddsReleasesToEndOfFile(t *testing.T) {
+	setUp()
+	defer tearDown()
+	copyFile(knownReleasesFile(), path.Join(testData(), "3knownreleases"))
+	currentScan := []FolderInfo{
+		{DummyFileInfo{"Vent", true}, DummyFileInfo{"Daniel Menche", true}},
+		{DummyFileInfo{"Iconoclastic Diaries", true}, DummyFileInfo{"Shake", true}},
+	}
+	UpdateKnownReleases(currentScan, knownReleasesFile(), 0)
+
+	file, lines := knownReleasesLines()
+	defer file.Close()
+
+	expectInt(t, "number of known releases", 5, len(lines))
+	expect(t, "new release 1", "Shake - Iconoclastic Diaries", lines[len(lines)-2])
+	expect(t, "new release 2", "Daniel Menche - Vent", lines[len(lines)-1])
+}
+
+func TestItIgnoresReleasesAlreadyKnown(t *testing.T) {
+	setUp()
+	defer tearDown()
+	copyFile(knownReleasesFile(), path.Join(testData(), "3knownreleases"))
+	currentScan := []FolderInfo{
+		{DummyFileInfo{"Vent", true}, DummyFileInfo{"Daniel Menche", true}},
+		{DummyFileInfo{"I Do, I Do, I Do, I Do, I Do", true}, DummyFileInfo{"Abba", true}},
+		{DummyFileInfo{"Iconoclastic Diaries", true}, DummyFileInfo{"Shake", true}},
+		{DummyFileInfo{"Discipline", true}, DummyFileInfo{"Throbbing Gristle", true}},
+	}
+	UpdateKnownReleases(currentScan, knownReleasesFile(), 0)
+
+	file, lines := knownReleasesLines()
+	defer file.Close()
+
+	expectInt(t, "number of known releases", 5, len(lines))
+	expect(t, "new release 1", "Shake - Iconoclastic Diaries", lines[len(lines)-2])
+	expect(t, "new release 2", "Daniel Menche - Vent", lines[len(lines)-1])
+}
 
 func setUp() {
 	os.Mkdir(tempDir(), os.ModePerm)
@@ -27,6 +64,15 @@ func tearDown() {
 
 func knownReleasesFile() string {
 	return path.Join(tempDir(), "knownreleases")
+}
+
+func knownReleasesLines() (file *os.File, lines []string) {
+	file, _ = os.Open(knownReleasesFile())
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return file, lines
 }
 
 func testData() string {

47b4d14ff06f555e68c4c15f33ea607e5cc6b8bb
epond <epond@users.noreply.github.com>
2016-08-14 11:40:15 +0100

    remove redundant FileInfo sorting

--- folderScanner.go
+++ folderScanner.go
@@ -6,46 +6,14 @@ import (
 	"fmt"
 	"log"
 	"unicode/utf8"
-	"sort"
 )
 
 func LatestAdditions(musicFolder string) string {
-	allFileInfos := append(
-		FileInfoAtDepth(path.Join(musicFolder, "flac-add"), 2),
-		FileInfoAtDepth(path.Join(musicFolder, "flac-vorbis320"), 2)...)
-	latestFileInfos := LatestFileInfos(allFileInfos, 3)
-	return fmt.Sprintf("%v, %v, %v", latestFileInfos[0].Name(), latestFileInfos[1].Name(), latestFileInfos[2].Name())
-}
-
-func FileInfoAtDepth(rootFolderPath string, targetDepth int) []os.FileInfo {
-	if targetDepth <= 0 {
-		return []os.FileInfo{}
-	}
-
-	rootFile, err := os.Open(rootFolderPath)
-	if err != nil {
-		log.Fatalf("Could not open root folder. Cause: %v", err)
-	}
-	children, err := rootFile.Readdir(0)
-	if err != nil {
-		log.Fatalf("Could not read root folder info. Cause: %v", err)
-	}
-
-	fileInfos := make([]os.FileInfo, 0)
-
-	for _, child := range children {
-		firstChar, _ := utf8.DecodeRuneInString(child.Name())
-		if child.IsDir() && child.Name() != "@eaDir" && firstChar != '.' {
-			if targetDepth == 1 {
-				fileInfos = append(fileInfos, child)
-			} else {
-				childFileInfos := FileInfoAtDepth(path.Join(rootFolderPath, child.Name()), targetDepth - 1)
-				fileInfos = append(fileInfos, childFileInfos...)
-			}
-		}
-	}
-
-	return fileInfos
+	folderScanList := append(
+		FolderInfoAtDepth(path.Join(musicFolder, "flac-add"), 2),
+		FolderInfoAtDepth(path.Join(musicFolder, "flac-vorbis320"), 2)...)
+	// This will eventually show the last n entries from the latest additions file instead
+	return fmt.Sprintf("%v, %v, %v", folderScanList[0].String(), folderScanList[1].String(), folderScanList[2].String())
 }
 
 func FolderInfoAtDepthIter(rootFolderPath string, targetDepth int, parent os.FileInfo) []FolderInfo {
@@ -88,33 +56,6 @@ func FolderInfoAtDepthIter(rootFolderPath string, targetDepth int, parent os.Fil
 
 func FolderInfoAtDepth(rootFolderPath string, targetDepth int) []FolderInfo {
 	return FolderInfoAtDepthIter(rootFolderPath, targetDepth, nil)
-}
-
-func LatestFileInfos(fileInfos []os.FileInfo, limit int) []os.FileInfo {
-	sortedFileInfos := sortByModTime(fileInfos)
-	if len(sortedFileInfos) > limit {
-		return sortedFileInfos[:limit]
-	}
-	return sortedFileInfos
-}
-
-type FileInfosSortedByModifiedTime []os.FileInfo
-
-func (slice FileInfosSortedByModifiedTime) Len() int {
-	return len(slice)
-}
-
-func (slice FileInfosSortedByModifiedTime) Less(i, j int) bool {
-	return slice[i].ModTime().After(slice[j].ModTime())
-}
-
-func (slice FileInfosSortedByModifiedTime) Swap(i, j int) {
-	slice[i], slice[j] = slice[j], slice[i]
-}
-
-func sortByModTime(fileInfos FileInfosSortedByModifiedTime) FileInfosSortedByModifiedTime {
-	sort.Sort(fileInfos)
-	return fileInfos
 }
 
 type FolderInfo struct {
--- folderScanner_test.go
+++ folderScanner_test.go
@@ -7,48 +7,10 @@ import (
 	"strconv"
 	"bytes"
 	"sort"
-	"time"
 )
 
 func TestGivenZeroDepthThenReturnEmptyArray(t *testing.T) {
-	expectInt(t, "number of fileinfos", 0, len(FileInfoAtDepth("anything", 0)))
-}
-
-func TestGivenDepthOfOneThenReturnSubfolderInfo(t *testing.T) {
-	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
-	fileInfos := sortByName(FileInfoAtDepth(folderPath, 1))
-	expectInt(t, "number of fileinfos", 3, len(fileInfos))
-	expect(t, "foldernames", "a2|b2|c2", fileInfoNames(fileInfos))
-}
-
-func TestGivenDepthOfTwoThenReturnSubfolderInfo(t *testing.T) {
-	folderPath := path.Join(os.Getenv("GOPATH"), "src/github.com/epond/porthole/testdata/a1")
-	fileInfos := sortByName(FileInfoAtDepth(folderPath, 2))
-	expectInt(t, "number of fileinfos", 4, len(fileInfos))
-	expect(t, "folder names", "a3b2|a3c2|b3b2|b3c2", fileInfoNames(fileInfos))
-}
-
-func TestLatestFileInfosLimitsNumberOfResults(t *testing.T) {
-	fileInfos := []os.FileInfo{
-		DummyFileInfo{"file1", 1, os.ModeDir, time.Now(), true},
-		DummyFileInfo{"file2", 1, os.ModeDir, time.Now(), true},
-		DummyFileInfo{"file3", 1, os.ModeDir, time.Now(), true},
-	}
-	expectInt(t, "length of latest fileInfos", 2, len(LatestFileInfos(fileInfos, 2)))
-	expectInt(t, "length of latest fileInfos", 3, len(LatestFileInfos(fileInfos, 3)))
-	expectInt(t, "length of latest fileInfos", 3, len(LatestFileInfos(fileInfos, 4)))
-}
-
-func TestLatestFileInfosOrdersByModifiedTime(t *testing.T) {
-	now := time.Now()
-	rawFileInfos := []os.FileInfo{
-		DummyFileInfo{"file1", 1, os.ModeDir, now.Add(12 * time.Hour), true},
-		DummyFileInfo{"file2", 1, os.ModeDir, now, true},
-		DummyFileInfo{"file3", 1, os.ModeDir, now.Add(3 * time.Hour), true},
-		DummyFileInfo{"file4", 1, os.ModeDir, now.Add(9 * time.Hour), true},
-		DummyFileInfo{"file5", 1, os.ModeDir, now.Add(2 * time.Hour), true},
-	}
-	expect(t, "folder names", "file1|file4|file3", fileInfoNames(LatestFileInfos(rawFileInfos, 3)))
+	expectInt(t, "number of folderInfos", 0, len(FolderInfoAtDepth("anything", 0)))
 }
 
 // A folder such as a/b needs to have entry "a - b" where a is artist and b is name of release
@@ -76,17 +38,6 @@ func expectInt(t *testing.T, valueName string, expected int, actual int) {
 	expect(t, valueName, strconv.Itoa(expected), strconv.Itoa(actual))
 }
 
-func fileInfoNames(fileInfos []os.FileInfo) string {
-	var allnames bytes.Buffer
-	for i, fileInfo := range fileInfos {
-		allnames.WriteString(fileInfo.Name())
-		if i < len(fileInfos) - 1 {
-			allnames.WriteString("|")
-		}
-	}
-	return allnames.String()
-}
-
 func pipeDelimitedString(list []FolderInfo) string {
 	var all bytes.Buffer
 	for i, element := range list {
@@ -96,57 +47,6 @@ func pipeDelimitedString(list []FolderInfo) string {
 		}
 	}
 	return all.String()
-}
-
-type FileInfosSortedByName []os.FileInfo
-
-func (slice FileInfosSortedByName) Len() int {
-	return len(slice)
-}
-
-func (slice FileInfosSortedByName) Less(i, j int) bool {
-	return slice[i].Name() < slice[j].Name();
-}
-
-func (slice FileInfosSortedByName) Swap(i, j int) {
-	slice[i], slice[j] = slice[j], slice[i]
-}
-
-func sortByName(fileInfos FileInfosSortedByName) FileInfosSortedByName {
-	sort.Sort(fileInfos)
-	return fileInfos
-}
-
-type DummyFileInfo struct {
-	name string
-	size int64
-	mode os.FileMode
-	modTime time.Time
-	isDir bool
-}
-
-func (d DummyFileInfo) Name() string {
-	return d.name
-}
-
-func (d DummyFileInfo) Size() int64 {
-	return d.size
-}
-
-func (d DummyFileInfo) Mode() os.FileMode {
-	return d.mode
-}
-
-func (d DummyFileInfo) ModTime() time.Time {
-	return d.modTime
-}
-
-func (d DummyFileInfo) IsDir() bool {
-	return d.isDir
-}
-
-func (d DummyFileInfo) Sys() interface{} {
-	return nil
 }
 
 type FolderInfosSortedByString []FolderInfo

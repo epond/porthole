e736811b40ef53ebbeab80df24d2e2d2b8afff27
epond <epond@users.noreply.github.com>
2016-08-15 07:52:30 +0100

    planning in comments

--- releasesRepo.go
+++ releasesRepo.go
@@ -9,6 +9,14 @@ func UpdateKnownReleases(folderScanList []FolderInfo, knownReleasesFile string, 
 		defer file.Close()
 	}
 
+	// Build a map of known releases
+	// Build a map of the current scan
+	// Build a list of current scan entries not present in known releases (new releases)
+	// Sort new releases by name
+	// Append new releases to known releases file
+
+	// ?How should latest n releases be determined?
+
 	if len(folderScanList) >= 3 {
 		return []string{folderScanList[0].String(), folderScanList[1].String(), folderScanList[2].String()}
 	}

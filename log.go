e7239cb59ff6ef385931635c7bc3097fcba70762
epond <epond@users.noreply.github.com>
2016-08-27 12:30:34 +0100

    message if log file missing

--- log.go
+++ log.go
@@ -4,12 +4,17 @@ import (
 	"net/http"
 	"io/ioutil"
 	"html/template"
+	"fmt"
 )
 
 func logHandler(logFile string) func(res http.ResponseWriter, _ *http.Request) {
 	return func(res http.ResponseWriter, req *http.Request) {
 		logTemplate, _ := template.ParseFiles(templatePath("log.html"))
 		logFileBytes, _ := ioutil.ReadFile(logFile)
-		logTemplate.Execute(res, string(logFileBytes))
+		if len(logFileBytes) > 0 {
+			logTemplate.Execute(res, string(logFileBytes))
+		} else {
+			logTemplate.Execute(res, fmt.Sprintf("there is no log at %v", logFile))
+		}
 	}
 }

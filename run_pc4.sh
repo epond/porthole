4c0d010cc00de0313d5b9ed038d402808cc3e11f
epond <epond@users.noreply.github.com>
2016-09-25 12:00:14 +0100

    pc4 run script mounts nas folders

--- run_pc4.sh
+++ run_pc4.sh
@@ -10,7 +10,15 @@ export FOLDERS_TO_SCAN=flac:3,flac-cd:3,flac-add:2,flac-vorbis320:2,mp3/main:2
 export LATEST_ADDITIONS_LIMIT=50
 export GIT_COMMIT=`git log --pretty=format:'%h' -n 1`
 
+echo "Mounting media folder from nas"
 rm /home/ed/porthole.log
+touch /home/ed/porthole.log
+sudo mkdir -p /mnt/nasmedia
+sudo mkdir -p /mnt/dashboard
+sudo chmod 777 /mnt/nasmedia
+sudo chmod 777 /mnt/dashboard
+sudo sh -c 'mount -t cifs //192.168.1.102/media /mnt/nasmedia --verbose -o credentials=/home/ed/nascredentials >> /home/$
+sudo sh -c 'mount -t cifs //192.168.1.102/dashboard /mnt/dashboard --verbose -o credentials=/home/ed/nascredentials >> /$
 
 echo "Starting porthole in background..."
 /home/ed/go/bin/porthole >> /home/ed/porthole.log 2>&1 &

25124bb697340803cf502d69662ab9bf9ffbd15a
epond <epond@users.noreply.github.com>
2019-10-21 08:54:16 +0100

    run script uses testdata

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-export MUSIC_FOLDER="/Users/edpond/Documents/porthole"
-export KNOWN_RELEASES_FILE="/Users/edpond/Documents/porthole/knownreleases.txt"
+export MUSIC_FOLDER="$GOPATH/src/github.com/epond/porthole"
+export KNOWN_RELEASES_FILE="$GOPATH/src/github.com/epond/porthole/knownreleases.txt"
 export KNOWN_RELEASES_BACKUP=$MUSIC_FOLDER"/knownreleases_backup.txt"
 export GIT_COMMIT=`git log --pretty=format:'%h' -n 1`
-export LOG_FILE="/Users/edpond/Documents/porthole/porthole.log"
+export LOG_FILE="$GOPATH/src/github.com/epond/porthole/porthole.log"
 export FETCH_INTERVAL=1000
 export DASHBOARD_REFRESH_INTERVAL=1000
-export SLEEP_AFTER=10000
+export SLEEP_AFTER=3000
 export LATEST_ADDITIONS_LIMIT=10
-export FOLDERS_TO_SCAN="dummymusic/flac-vorbis320:2,dummymusic/mp3/main:2,dummymusic/hd audio:3"
+export FOLDERS_TO_SCAN="testdata:2"
 
 ./build.sh &&
 $GOPATH/bin/porthole
